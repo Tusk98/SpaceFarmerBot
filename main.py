0a37ff62aa739ae43ef728a807c22b48729ee1cd
Tusk98 <henryracoomb@gmail.com>
2019-05-11 15:37:00 -0400

    Changed code to discord extension.
     - add toml configurations

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import sys
+import asyncio
+
 import discord
+import toml
+import xdg
+from discord.ext import commands
 
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+PROGRAM_NAME = "SpaceFarmerBot"
+CONFIG_FILE = "config.toml"
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+# initialize client
+client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
-    print('-----------')
+    print('------')
 
-client.run(TOKEN)
+if (__name__ == "__main__"):
+    config_dir = xdg.XDG_CONFIG_HOME/PROGRAM_NAME
+    config_file = config_dir/CONFIG_FILE
+    if (config_file.exists()):
+        parsed_toml = {}
+        with open(config_file) as f:
+            parsed_toml = toml.load(f)
+
+        if ("token" in parsed_toml):
+            client.run(parsed_toml["token"])
+        else:
+            print("No bot token configured!")
+    else:
+        print("No configuration found!")
+
