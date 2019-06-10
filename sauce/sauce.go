f0a236cdeedb1d29e0f2cb0a116839e28cb6b051
Jiayi Zhao <jeff.no.zhao@gmail.com>
2019-06-10 17:41:02 -0400

    add support for showing image similarity of results

--- sauce/sauce.go
+++ sauce/sauce.go
@@ -17,57 +17,154 @@ func (self *GenericBotError) Error() string {
     return self.reason
 }
 
+type SimilarityResult struct {
+    URL string
+    PercentSimilar uint8
+    Height uint
+    Width uint
+}
+
 const Command string = "sauce"
 const COLOR int = 0xff93ac
 
+const IQDB_PATTERN string = "match</th></tr><tr><td class='image'><a href=\""
+const IQDB_DIMENSION_PATTERN string = "class=\"service-icon\">"
+
 func ProcessCommand(s *discordgo.Session, m *discordgo.MessageCreate, args string) error {
-    if len(m.Attachments) == 0 {
-        s.ChannelMessageSend(m.ChannelID, "No images to work off of? Back to farming...")
+    if len(m.Attachments) == 0 && len(args) == 0 {
+        s.ChannelMessageSend(m.ChannelID, "No images/links to work off of? Back to farming...")
         return nil
     }
 
-    const pattern string = "match</th></tr><tr><td class='image'><a href=\""
+    for _, arg := range strings.Split(args, " ") {
+        if len(arg) <= 6 || !strings.HasPrefix(arg, "http") {
+            continue
+        }
 
-    spaceClient := http.Client{}
-    for _, attachment := range m.Attachments {
-        resp, err := spaceClient.PostForm("http://iqdb.org",
-                url.Values{"url": { attachment.URL }})
+        results, err := getSimilarResults(arg)
         if err != nil {
             return err
         }
-        if resp.StatusCode != 200 {
-            return &GenericBotError{
-                reason: fmt.Sprintf("Request failed with error code: %d", resp.StatusCode),
-            }
+        if len(results) == 0 {
+            s.ChannelMessageSend(m.ChannelID, "No results found")
+            continue
         }
-        defer resp.Body.Close()
-
-        body, err := ioutil.ReadAll(resp.Body)
-        web_html := string(body)
 
         var buffer bytes.Buffer
-        yet_to_parse_html := web_html
-        for i := strings.Index(yet_to_parse_html, pattern); i != -1; i = strings.Index(yet_to_parse_html, pattern) {
-            yet_to_parse_html = yet_to_parse_html[i+len(pattern):]
-            idx := strings.IndexRune(yet_to_parse_html, '"')
-            if idx == -1 {
-                break
+        fields := []*discordgo.MessageEmbedField{}
+        for i, result := range results {
+            field := &discordgo.MessageEmbedField{
+                Name: fmt.Sprintf("%d: Similarity: %d%%", i+1, result.PercentSimilar),
+                Value: result.URL,
             }
-            url := yet_to_parse_html[:idx]
-            if !strings.HasPrefix(url, "http") {
-                buffer.WriteString("https:")
-            }
-            buffer.WriteString(url)
-            buffer.WriteString("\n")
+            fields = append(fields, field)
         }
 
         embed := &discordgo.MessageEmbed {
                 Title: "Results",
                 Color: COLOR,
                 Description: buffer.String(),
+                Thumbnail: &discordgo.MessageEmbedThumbnail{
+                    URL: arg,
+                },
+                Fields: fields,
             }
         s.ChannelMessageSendEmbed(m.ChannelID, embed)
+    }
 
+    for _, attachment := range m.Attachments {
+        results, err := getSimilarResults(attachment.URL)
+        if err != nil {
+            return err
+        }
+
+        var buffer bytes.Buffer
+        for _, result := range results {
+            buffer.WriteString(result.URL)
+            buffer.WriteRune('\n')
+        }
+        embed := &discordgo.MessageEmbed {
+                Title: "Results",
+                Color: COLOR,
+                Description: buffer.String(),
+            }
+        s.ChannelMessageSendEmbed(m.ChannelID, embed)
     }
     return nil
+}
+
+func getSimilarResults(img_url string) ([]SimilarityResult, error) {
+    resp, err := http.PostForm("http://iqdb.org",
+                url.Values{"url": { img_url }})
+    if err != nil {
+        return nil, err
+    }
+    if resp.StatusCode != 200 {
+        return nil, &GenericBotError{
+            reason: fmt.Sprintf("Request failed with error code: %d", resp.StatusCode),
+        }
+    }
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    web_html := string(body)
+
+    return parseResultHTML(web_html), nil
+}
+
+func parseResultHTML(body string) []SimilarityResult {
+    results := []SimilarityResult{}
+
+    yet_to_parse := body
+    for i := strings.Index(yet_to_parse, IQDB_PATTERN); i != -1; i = strings.Index(yet_to_parse, IQDB_PATTERN) {
+        yet_to_parse = yet_to_parse[i + len(IQDB_PATTERN):]
+        idx := strings.IndexRune(yet_to_parse, '"')
+        if idx == -1 {
+            break
+        }
+        url := yet_to_parse[:idx]
+
+        var percent uint8
+        var width uint
+        var height uint
+
+        const IQDB_XY_PATTERN_1 string = "class=\"service-icon\">"
+        idx = strings.Index(yet_to_parse, IQDB_XY_PATTERN_1)
+        if idx == -1 {
+            continue
+        }
+        yet_to_parse = yet_to_parse[idx + len(IQDB_XY_PATTERN_1):]
+
+        const IQDB_XY_PATTERN_2 string = "<td>"
+        idx = strings.Index(yet_to_parse, IQDB_XY_PATTERN_2)
+        if idx == -1 {
+            continue
+        }
+        yet_to_parse = yet_to_parse[idx + len(IQDB_XY_PATTERN_2):]
+        fmt.Sscanf(yet_to_parse, "%d×%d", &width, &height)
+
+        const IQDB_SIM_PATTERN_1 string = "<td>"
+        idx = strings.Index(yet_to_parse, IQDB_SIM_PATTERN_1)
+        if idx == -1 {
+            continue
+        }
+        yet_to_parse = yet_to_parse[idx + len(IQDB_SIM_PATTERN_1):]
+
+        fmt.Sscanf(yet_to_parse, "%d%% similarity", &percent)
+
+        // some urls might not be complete
+        if !strings.HasPrefix(url, "http") {
+            url = fmt.Sprintf("https:%s", url)
+        }
+
+        result := SimilarityResult {
+            URL: url,
+            Width: width,
+            Height: height,
+            PercentSimilar: percent,
+        }
+        fmt.Printf("%+v\n", result)
+        results = append(results, result)
+    }
+    return results
 }
